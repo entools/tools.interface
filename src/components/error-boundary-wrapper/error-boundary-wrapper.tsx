d730d41c93d5159ccebb7f04253ab5f050b2cd52
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-10-20 22:07:36 +0300

    feat: error-boundary-wrapper

---
+++ src/components/error-boundary-wrapper/error-boundary-wrapper.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-children-prop */
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+import type { PropsWithChildren } from 'react';
+import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
+import classNames from 'classnames';
+
+import Content from '../content/index.tsx';
+// import Button from '../../ui/button';
+
+import style from './error-boundary.module.css';
+
+type ErrorBoundaryWrapperProps = PropsWithChildren<unknown>;
+
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <Content
+      children={(
+        <div className={style.boundary}>
+          <h2 className={style.title}>APP-ERROR</h2>
+          <p className={style.message}>{error.message}</p>
+          <div className={style.block}>
+            Try to
+            <button
+              type="button"
+              className={style.button}
+              onClick={resetErrorBoundary}
+            >
+              Reload app
+            </button>
+            or
+            <button
+              type="button"
+              className={classNames('link', style.link)}
+              onClick={resetErrorBoundary}
+            >
+              Go to homepage
+            </button>
+          </div>
+        </div>
+      )}
+    />
+  );
+}
+
+export default function ErrorBoundaryWrapper({ children }: ErrorBoundaryWrapperProps) {
+  return (
+    <ErrorBoundary onReset={() => console.log('reset')} FallbackComponent={ErrorFallback}>
+      {children}
+    </ErrorBoundary>
+  );
+}
