d09e5b9ebb4c357a48c65281530fa1f253d80391
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-04 00:21:24 +0300

    fix: profile-page

--- src/components/sidebar/components/profile/profile.tsx
+++ src/components/sidebar/components/profile/profile.tsx
@@ -1,17 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+
 import style from './profile.module.css';
 
 export default function Profile({ sidebarWidth }: { sidebarWidth: number; }) {
   return (
-    <div className={style.profile}>
-      <div className={style.pic}>a</div>
+    <div className={classNames(style.profile, { [style.column]: sidebarWidth < 120 })}>
+      <NavLink to="/profile" className={style.pic}>
+        a
+      </NavLink>
       <div className={style.name}>
         {sidebarWidth >= 120 && 'footer'}
       </div>
-      {sidebarWidth >= 120 && (
-        <div className={style.notification}>
-          !
-        </div>
-      )}
+      <div className={style.notification}>
+        !
+      </div>
     </div>
   );
 }
