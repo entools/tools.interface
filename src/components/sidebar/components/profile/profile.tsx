0486d1d96a553e10e66139035a175ef11a35a067
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-03 16:06:40 +0300

    fix: sidebar & ci

--- src/components/sidebar/components/profile/profile.tsx
+++ src/components/sidebar/components/profile/profile.tsx
@@ -5,8 +5,13 @@ export default function Profile({ sidebarWidth }: { sidebarWidth: number; }) {
     <div className={style.profile}>
       <div className={style.pic}>a</div>
       <div className={style.name}>
-        {sidebarWidth >= 100 && 'footer'}
+        {sidebarWidth >= 120 && 'footer'}
       </div>
+      {sidebarWidth >= 120 && (
+        <div className={style.notification}>
+          !
+        </div>
+      )}
     </div>
   );
 }
