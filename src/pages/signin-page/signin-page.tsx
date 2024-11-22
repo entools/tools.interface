f9576260c41eb109913add430a55a4a4dead9c2d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-22 22:56:08 +0300

    fix: signin & signup page

--- src/pages/signin-page/signin-page.tsx
+++ src/pages/signin-page/signin-page.tsx
@@ -1,6 +1,22 @@
 function SigninPage() {
   return (
-    <h1>Signin</h1>
+    <>
+      <h1>Signin</h1>
+      <input
+        className="input"
+        placeholder="login"
+      />
+      <input
+        className="input"
+        placeholder="password"
+      />
+      <button
+        type="button"
+        className="button"
+      >
+        signin
+      </button>
+    </>
   );
 }
 
--- src/pages/signup-page/signup-page.tsx
+++ src/pages/signup-page/signup-page.tsx
@@ -1,6 +1,22 @@
 function SignupPage() {
   return (
-    <h1>Signup</h1>
+    <>
+      <h1>Signup</h1>
+      <input
+        className="input"
+        placeholder="login"
+      />
+      <input
+        className="input"
+        placeholder="password"
+      />
+      <button
+        type="button"
+        className="button"
+      >
+        signup
+      </button>
+    </>
   );
 }
 
