825e6dcadfe9485c6e0323d2dd3203827729654a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-22 18:01:55 +0300

    fix: css

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Route, Routes } from 'react-router-dom';
 
 import { useAppLocation } from './hooks/use-app-location.tsx';
@@ -11,16 +10,16 @@ import DocumentPage from './pages/document-page/document-page.tsx';
 import ProfilePage from './pages/profile-page/profile-page.tsx';
 import OauthPage from './pages/oauth-page/oauth-page.tsx';
 import SigninPage from './pages/signin-page/signin-page.tsx';
+import SignupPage from './pages/signup-page/signup-page.tsx';
 import SearchPage from './pages/search-page/search-page.tsx';
+import ConfirmEmailPage from './pages/confirm-email-page/confirm-email-page.tsx';
+import ResetPasswordPage from './pages/reset-password-page/reset-password-page.tsx';
 import ProjectAdd from './pages/project-add-page/project-add-page.tsx';
 import UsersPage from './pages/users-page/users-page.tsx';
 import NotFoundPage from './pages/not-found-page/not-found-page.tsx';
 
-import './app.css';
-
-function App() {
+export default function App() {
   const location = useAppLocation();
-  // const background = location.state?.pathname;
   const background = location.state?.pathname;
 
   return (
@@ -33,10 +32,11 @@ function App() {
             <Route path="/projects/:projectId/documents/:documentId" element={(<DocumentPage />)} />
             <Route path="/projects/add" element={(<ProjectAdd />)} />
           </Route>
-
           <Route path="/oauth" element={(<OauthPage />)} />
           <Route path="/signin" element={(<SigninPage />)} />
-          {/* <Route path="/search" element={(<SearchPage />)} /> */}
+          <Route path="/signup" element={(<SignupPage />)} />
+          <Route path="/reset-password" element={(<ResetPasswordPage />)} />
+          <Route path="/confirm-email" element={(<ConfirmEmailPage />)} />
           <Route path="*" element={(<NotFoundPage />)} />
         </Routes>
 
@@ -51,5 +51,3 @@ function App() {
     </ThemeProvider>
   );
 }
-
-export default App;
