dc12920cdfa48401017250ac71488434ee307fe9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-22 18:50:47 +0300

    fix: project-add page

--- src/pages/project-add-page/project-add-page.tsx
+++ src/pages/project-add-page/project-add-page.tsx
@@ -3,23 +3,29 @@ import style from './project-add-page.module.css';
 function ProjectPage() {
   return (
     <div className="layout">
-      <h2 className={style.title}>
+      <h2 className="title">
         Add project
       </h2>
       <div className={style.form}>
         <div className={style.main}>
           <input
-            className={style.input}
+            className="input"
             placeholder="Name"
           />
           <input
-            className={style.input}
+            className="input"
             placeholder="Tag"
           />
           <input
-            className={style.input}
+            className="input"
             placeholder="Details"
           />
+          <button
+            type="button"
+            className="button"
+          >
+            save
+          </button>
         </div>
       </div>
     </div>
