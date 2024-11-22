b2f5d058808bfabeee6d33193914877f173a2a9c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-22 22:53:44 +0300

    feat: dnd

--- src/pages/document-page/document-page.tsx
+++ src/pages/document-page/document-page.tsx
@@ -1,28 +1,64 @@
-import {
-  useParams,
-} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import style from './document-page.module.css';
 
 function DocumentPage() {
   const { projectId, documentId } = useParams();
 
   return (
-    <div className="layout">
-      <h2 className="title">
-        {`Document ${documentId} #${projectId}`}
-      </h2>
-      <div className={style.tools}>Tools</div>
-      <div className={style.form}>
-        <div className={style.main}>
-          <div>Name</div>
-          <div>Name block</div>
-          <div>Name item</div>
-        </div>
-        <div className={style.history}>
-          History
+    <DndProvider backend={HTML5Backend}>
+      <div className="layout">
+        <h2 className="title">
+          {`Document ${documentId} #${projectId}`}
+        </h2>
+        <div className={style.tools}>Tools</div>
+        <div className={style.form}>
+          <div className={style.main}>
+            <input
+              className="input"
+              placeholder="Name"
+            />
+            <div className={style.block}>
+              <input
+                className="input"
+                placeholder="Name block"
+              />
+              <div className={style.items}>
+                <input
+                  className="input"
+                  placeholder="Name item"
+                />
+                <input
+                  className="input"
+                  placeholder="Name item"
+                />
+                <input
+                  className="input"
+                  placeholder="Name item"
+                />
+              </div>
+            </div>
+            <div className={style.block}>
+              <input
+                className="input"
+                placeholder="Name block"
+              />
+              <div className={style.items}>
+                <input
+                  className="input"
+                  placeholder="Name item"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={style.history}>
+            History
+          </div>
         </div>
       </div>
-    </div>
+    </DndProvider>
   );
 }
 
