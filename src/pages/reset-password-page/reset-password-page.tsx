83a7e27383baf0bc2e83dddde66e9b464ac244dd
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-22 22:55:45 +0300

    fix: reset-password-page

--- src/pages/reset-password-page/reset-password-page.tsx
+++ src/pages/reset-password-page/reset-password-page.tsx
@@ -1,6 +1,18 @@
 function ResetPasswordPage() {
   return (
-    <h1>Reset password page</h1>
+    <>
+      <h1>Reset password page</h1>
+      <input
+        className="input"
+        placeholder="email"
+      />
+      <button
+        type="button"
+        className="button"
+      >
+        reset
+      </button>
+    </>
   );
 }
 
