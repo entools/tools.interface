8d3e78d6239067e49f64482bd9da5ef82954c47b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-26 22:07:18 +0300

    fix: profile

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
+import { CustomSelect as Select } from '../../components/select/select.tsx';
+
 import style from './profile-page.module.css';
 
+type SelectType = { value: string; label: string; };
+
+const options = [
+  { value: 'sytem', label: 'sytem' },
+  { value: 'dark', label: 'dark' },
+  { value: 'light', label: 'light' },
+];
+
 function ProfilePage() {
+  // const initTheme = localStorage.getItem('theme');
+  const [theme, setTheme] = useState<SelectType | null>(null);
+
+  useEffect(() => {
+    if (theme) {
+      if (theme.value === 'light') {
+        document.documentElement.setAttribute('data-theme', 'light');
+      } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-theme', theme.value);
+      }
+    }
+  }, [theme]);
+
   return (
     <div className="layout">
       <h2 className="title">
@@ -29,8 +55,21 @@ function ProfilePage() {
           </button>
         </div>
         <div className={style.settings}>
-          <div className={style.theme}>theme</div>
-          <div className={style.notification}>notification</div>
+          <div className={style.theme}>
+            {`theme ${theme?.value}`}
+            <div className={style.select}>
+              <Select
+                name="theme"
+                options={options}
+                action={setTheme}
+                value={theme}
+              />
+            </div>
+
+          </div>
+          <div className={style.notification}>
+            notification
+          </div>
           <div className={style.base}>base</div>
         </div>
 
