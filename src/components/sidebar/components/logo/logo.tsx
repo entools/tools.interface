f2b5c92787e819cb5984c1534b0786261d679cac
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-26 07:37:44 +0300

    fix: logo

--- src/components/sidebar/components/logo/logo.tsx
+++ src/components/sidebar/components/logo/logo.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react/function-component-definition */
-/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-console */
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import Select, { type DropdownIndicatorProps, components } from 'react-select';
 import classNames from 'classnames';
-import { IoIosAdd, IoIosArrowDown, IoMdHome } from 'react-icons/io';
+import { IoIosAdd, IoMdHome } from 'react-icons/io';
+
+import { CustomSelect as Select } from '../../../select/select.tsx';
 
 import style from './logo.module.css';
 
@@ -17,21 +16,17 @@ export interface ColourOption {
   readonly isDisabled?: boolean;
 }
 
+type SelectType = { value: string; label: string; };
+
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' },
 ];
 
-const Placeholder = (props: any) => <components.Placeholder {...props} />;
-
-const DropdownIndicator: React.FC<DropdownIndicatorProps> = (props) => (
-  <components.DropdownIndicator {...props}>
-    <IoIosArrowDown size={14} />
-  </components.DropdownIndicator>
-);
-
 export default function Logo({ sidebarWidth }: { sidebarWidth: number; }) {
+  const [project, setProject] = useState<SelectType | null>(null);
+  console.log(project);
   return (
     <div className={classNames(style.header, { [style.small]: sidebarWidth < 100 })}>
       <div className={style.logo}>
@@ -42,31 +37,11 @@ export default function Logo({ sidebarWidth }: { sidebarWidth: number; }) {
         {sidebarWidth >= 178 && (
           <>
             <Select
-              className={classNames('basic-single', style.full)}
-              classNamePrefix="select"
-              components={{ Placeholder, DropdownIndicator }}
-              defaultValue={options[0]}
-              styles={{
-                control: (baseStyles) => ({
-                  ...baseStyles,
-                  minHeight: 20,
-                  marginLeft: '8px',
-                  border: 0,
-                }),
-                option: (styles) => ({ ...styles, color: '#ccc' }),
-              }}
-              theme={(theme) => ({
-                ...theme,
-                borderRadius: 8,
-                padding: 0,
-                colors: {
-                  ...theme.colors,
-                  primary25: '#757575',
-                  primary: '#616161',
-                },
-              })}
-              name="name"
+              name="project"
               options={options}
+              extraClass={{ marginLeft: '8px' }}
+              action={setProject}
+              value={project}
             />
             <NavLink to="/projects/add" className={style.icon}>
               <IoIosAdd />
