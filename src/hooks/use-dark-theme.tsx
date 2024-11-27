91989511e2e78c099ec3872df2211600d6853062
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-27 08:38:23 +0300

    fix: use-dark-theme

--- src/hooks/use-dark-theme.tsx
+++ src/hooks/use-dark-theme.tsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react';
 
 export default function useDarkTheme() {
-  const currentTheme = localStorage.getItem('ms-theme');
+  const currentTheme = localStorage.getItem('data-theme');
   const condition = currentTheme === 'dark' ? 'dark' : 'light';
   const [isDark, setIsDark] = useState(condition);
 
-  const toggleIsDark = () => {
-    setIsDark(isDark === 'light' ? 'dark' : 'light');
-    localStorage.setItem('ms-theme', isDark === 'light' ? 'dark' : 'light');
+  const toggleIsDark = (value: string) => {
+    setIsDark(value);
+
+    // if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+
+    // }
+    localStorage.setItem('data-theme', value);
+    // console.log(value);
+    // setIsDark(isDark === 'light' ? 'dark' : 'light');
+    // localStorage.setItem('data-theme', isDark === 'light' ? 'dark' : 'light');
   };
 
   const providerValue = { isDark, toggleIsDark };
 
-  useEffect(() => document.documentElement.setAttribute('ms-theme', condition), [isDark]);
+  useEffect(() => document.documentElement.setAttribute('data-theme', condition), [isDark]);
 
   return { providerValue };
 }
