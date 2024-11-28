321cd1d477ba103e3e8b862a454b725eaaf1ff55
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-11-28 21:18:12 +0300

    fix: signup

--- src/pages/signup-page/signup-page.tsx
+++ src/pages/signup-page/signup-page.tsx
@@ -1,22 +1,26 @@
+import style from './signup-page.module.css';
+
 function SignupPage() {
   return (
-    <>
-      <h1>Signup</h1>
-      <input
-        className="input"
-        placeholder="login"
-      />
-      <input
-        className="input"
-        placeholder="password"
-      />
-      <button
-        type="button"
-        className="button"
-      >
-        signup
-      </button>
-    </>
+    <div>
+      <div className={style.container}>
+        <h1>Signup</h1>
+        <input
+          className="input"
+          placeholder="login"
+        />
+        <input
+          className="input"
+          placeholder="password"
+        />
+        <button
+          type="button"
+          className="button"
+        >
+          signup
+        </button>
+      </div>
+    </div>
   );
 }
 
