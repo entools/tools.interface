cc33a523cbdabc2070f210d9794d6f60a13d2e25
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-12-09 22:15:53 +0300

    fix: document-page

--- src/pages/document-page/document-page.tsx
+++ src/pages/document-page/document-page.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import classNames from 'classnames';
 
 import style from './document-page.module.css';
 
 function DocumentPage() {
   const { projectId, documentId } = useParams();
+  const [history, setHistory] = useState(false);
 
   return (
     <DndProvider backend={HTML5Backend}>
@@ -13,8 +16,17 @@ function DocumentPage() {
         <h2 className="title">
           {`Document ${documentId} #${projectId}`}
         </h2>
-        <div className={style.tools}>Tools</div>
-        <div className={style.form}>
+        <div className={style.tools}>
+          Tools
+          <button
+            type="button"
+            className="button"
+            onClick={() => setHistory(!history)}
+          >
+            {history ? 'On' : 'Off'}
+          </button>
+        </div>
+        <div className={classNames(style.form, { [style.two]: history })}>
           <div className={style.main}>
             <input
               className="input"
@@ -53,9 +65,11 @@ function DocumentPage() {
               </div>
             </div>
           </div>
-          <div className={style.history}>
-            History
-          </div>
+          {history && (
+            <div className={style.history}>
+              History
+            </div>
+          )}
         </div>
       </div>
     </DndProvider>
