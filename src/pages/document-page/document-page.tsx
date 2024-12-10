9dc9953100c9d988b92d62bd4626eb9f53fe4c15
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-12-10 19:35:29 +0300

    fix: document-page

--- src/pages/document-page/document-page.tsx
+++ src/pages/document-page/document-page.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/button-has-type */
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import classNames from 'classnames';
+import {
+  IoIosAlbums, IoIosAdd, IoMdShareAlt, IoMdDownload, IoIosEye,
+} from 'react-icons/io';
 
 import style from './document-page.module.css';
 
@@ -18,13 +22,37 @@ function DocumentPage() {
         </h2>
         <div className={style.tools}>
           Tools
-          <button
-            type="button"
-            className="button"
-            onClick={() => setHistory(!history)}
-          >
-            {history ? 'On' : 'Off'}
-          </button>
+          <div>
+            <button
+              type="button"
+              className="button"
+              title="Подписаться"
+            >
+              <IoIosEye />
+            </button>
+            <button
+              type="button"
+              className="button"
+              title="Скачать"
+            >
+              <IoMdDownload />
+            </button>
+            <button
+              type="button"
+              className="button"
+              title="Share"
+            >
+              <IoMdShareAlt />
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={() => setHistory(!history)}
+              title={history ? 'On' : 'Off'}
+            >
+              <IoIosAlbums />
+            </button>
+          </div>
         </div>
         <div className={classNames(style.form, { [style.two]: history })}>
           <div className={style.main}>
@@ -51,6 +79,13 @@ function DocumentPage() {
                   placeholder="Name item"
                 />
               </div>
+              <button
+                type="button"
+                className="button"
+                title="Добавить элемент"
+              >
+                <IoIosAdd />
+              </button>
             </div>
             <div className={style.block}>
               <input
@@ -64,10 +99,21 @@ function DocumentPage() {
                 />
               </div>
             </div>
+            <button
+              type="button"
+              className="button"
+              title="Добавить блок"
+            >
+              <IoIosAdd />
+            </button>
           </div>
           {history && (
             <div className={style.history}>
-              History
+              <h3 className={style.title}>History</h3>
+              <ul className={style.list}>
+                <li className={style.item}>item 1</li>
+                <li className={style.item}>item 2</li>
+              </ul>
             </div>
           )}
         </div>
