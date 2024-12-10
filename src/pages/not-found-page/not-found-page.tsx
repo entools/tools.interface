639bf72f0d10bc9ec8ebb8b6d1cfaebc2786f90d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-12-10 07:44:07 +0300

    fix: not-found-page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -5,11 +5,17 @@ import style from './not-found-page.module.css';
 export default function NotFoundPage() {
   return (
     <div className={style.container}>
-      <h1>404</h1>
-      <h2>Page not found</h2>
-      <NavLink to="/">
-        Go to home
-      </NavLink>
+      <h1 className={style.title}>404</h1>
+      <h2 className={style.subtitle}>Нет такой страницы</h2>
+      <div className={style.text}>
+        {'Вы можете перейти на '}
+        <NavLink
+          to="/"
+          className={style.link}
+        >
+          главную
+        </NavLink>
+      </div>
     </div>
   );
 }
