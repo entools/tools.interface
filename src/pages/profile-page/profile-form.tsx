bf68a85c13fce5cdb9d1a1b9e2048698ed526e35
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-01-20 19:28:16 +0300

    fix: profile-page

--- src/pages/profile-page/profile-form.tsx
+++ src/pages/profile-page/profile-form.tsx
@@ -1,10 +1,70 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Controller, useForm } from 'react-hook-form';
 import classNames from 'classnames';
+
+import InputField from '../../components/input-field/input-field.tsx';
 
 import style from './profile-page.module.css';
 
-export default function ProfileForm() {
+export type FormPayload = Omit<User, 'id'>;
+
+const inputs = [
+  {
+    name: 'username',
+    label: 'Username',
+    pattern: {
+      value: /^[a-z0-9_-]{3,15}$/,
+      message: 'Username',
+    },
+    required: true,
+    autoComplete: 'username',
+  },
+  {
+    name: 'email',
+    label: 'Email',
+    pattern: {
+      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      message: 'Email is invalid',
+    },
+    required: true,
+    autoComplete: 'email',
+  },
+  {
+    name: 'firstName',
+    label: 'First name',
+    pattern: {
+      value: /^[a-zA-Zа-яА-ЯёЁ-]{3,15}$/,
+      message: 'First name is invalid',
+    },
+    required: true,
+    autoComplete: 'first-name',
+  },
+  {
+    name: 'lastName',
+    label: 'Last name',
+    pattern: {
+      value: /^[a-zA-Zа-яА-ЯёЁ-]{3,15}$/,
+      message: 'Last name is invalid',
+    },
+    required: true,
+    autoComplete: 'last-name',
+  },
+];
+
+export default function ProfileForm({ onSubmit }: { onSubmit: (data: FormPayload) => void }) {
+  const { control, handleSubmit } = useForm<FormPayload>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
   return (
-    <form className={style.account}>
+    <form
+      className={style.account}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className={style.avatar}>
         <div className={style.picture}>U</div>
         <input
@@ -13,19 +73,29 @@ export default function ProfileForm() {
         />
       </div>
 
-      <input
-        className="input"
-        placeholder="login"
-      />
-      <input
-        className="input"
-        placeholder="email"
-      />
+      {inputs.map((input) => (
+        <Controller
+          key={input.name}
+          name={input.name as keyof FormPayload}
+          rules={{
+            pattern: input.pattern,
+            required: input.required,
+          }}
+          control={control}
+          render={({ field, fieldState }) => (
+            <InputField
+              {...field}
+              {...input}
+              errorText={fieldState.error?.message}
+            />
+          )}
+        />
+      ))}
       <button
-        type="button"
-        className="button"
+        type="submit"
+        className={classNames('button', style.button)}
       >
-        save
+        Сохранить
       </button>
     </form>
   );
--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -3,14 +3,20 @@ import ProfileSettings from './profile-settings.tsx';
 
 import style from './profile-page.module.css';
 
+export type FormPayload = Omit<User, 'id'>;
+
 function ProfilePage() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onSubmit = async (data: FormPayload) => {
+  };
+
   return (
     <div className="layout">
       <h2 className="title">
         Profile
       </h2>
       <div className={style.form}>
-        <ProfileForm />
+        <ProfileForm onSubmit={onSubmit} />
         <ProfileSettings />
       </div>
     </div>
