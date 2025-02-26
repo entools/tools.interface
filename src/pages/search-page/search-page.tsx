5cb4c4429b937c0aeb077df8a3882146e879a216
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-02-26 21:40:50 +0300

    fix: search-page

--- src/pages/search-page/search-page.tsx
+++ src/pages/search-page/search-page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-children-prop */
 import { useCallback } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { Text } from '@gravity-ui/uikit';
 
 import Modal from '../../components/modal/modal';
 
@@ -10,7 +11,7 @@ function SearchPage() {
   const handleClose = useCallback(() => {
     navigate(location.state.pathname || '/');
   }, [location.state, navigate]);
-  return (<Modal title="Search" onClose={handleClose} children={(<h2>form</h2>)} overlay />);
+  return (<Modal title="Search" onClose={handleClose} children={(<Text variant="header-1">form</Text>)} overlay />);
 }
 
 export default SearchPage;
