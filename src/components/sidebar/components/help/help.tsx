bd920211dab7bdaad36806c4c273734454756dc4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-03-19 20:30:35 +0300

    fix: sidebar

--- src/components/sidebar/components/help/help.tsx
+++ src/components/sidebar/components/help/help.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router';
 
-import { Icon, Text } from '@gravity-ui/uikit';
+import { Icon, Text, Button } from '@gravity-ui/uikit';
 import { Bookmark } from '@gravity-ui/icons';
 
 import style from './help.module.css';
@@ -8,8 +8,14 @@ import style from './help.module.css';
 export default function Help({ sidebarWidth }: { sidebarWidth: number; }) {
   return (
     <NavLink to="/about" className={style.help}>
-      <Icon data={Bookmark} size={20} />
-      {sidebarWidth >= 148 && <Text variant="subheader-2">Support</Text>}
+      <Button
+        size="l"
+        view="outlined"
+        className={style.button}
+      >
+        <Icon data={Bookmark} size={20} />
+        {sidebarWidth >= 148 && <Text variant="subheader-2">Support</Text>}
+      </Button>
     </NavLink>
   );
 }
