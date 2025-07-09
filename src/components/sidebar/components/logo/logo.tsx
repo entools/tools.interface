63353d5e2212bc2e7e9159ab7d4d14549c307858
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-09 18:14:19 +0300

    fix: logo

--- src/components/sidebar/components/logo/logo.tsx
+++ src/components/sidebar/components/logo/logo.tsx
@@ -1,36 +1,44 @@
-/* eslint-disable no-console */
-import { useState } from 'react';
-import { NavLink } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router';
 
 import { Select, Icon } from '@gravity-ui/uikit';
 import { House, Plus } from '@gravity-ui/icons';
 
 import classNames from 'classnames';
 
+import { useAppSelector } from '~/hooks';
+import { useGetProjectsMutation } from '~/store';
+import { projectSelector } from '~/store/slices/project-slice';
+
 import style from './logo.module.css';
 
-export interface ColourOption {
-  readonly value: string;
-  readonly label: string;
-  readonly color: string;
-  readonly isFixed?: boolean;
-  readonly isDisabled?: boolean;
-}
+type SelectType = { value: string; content: string };
 
-type SelectType = { value: string; content: string; };
+export default function Logo({ sidebarWidth }: { sidebarWidth: number }) {
+  const navigate = useNavigate();
+  const [getProjects] = useGetProjectsMutation();
+  const projects = useAppSelector(projectSelector);
+  const options = React.useMemo(
+    () => projects.map(({ id, name }) => ({ value: id, content: name })),
+    [projects],
+  );
+  const [selectedProject, setSelectedProject] = useState<SelectType>();
 
-const options = [
-  { value: 'chocolate', content: 'Chocolate' },
-  { value: 'strawberry', content: 'Strawberry' },
-  { value: 'vanilla', content: 'Vanilla' },
-];
-
-export default function Logo({ sidebarWidth }: { sidebarWidth: number; }) {
-  const [project, setProject] = useState<SelectType>(options[0]);
-  const onChange = (v: string[]) => {
-    const current = options.find((x) => x.value === v[0]);
-    setProject(current || options[0]);
+  const handleProjectChange = ([id]: string[]) => {
+    if (id) {
+      navigate(`/projects/${id}/documents/1`);
+    }
   };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  useEffect(() => {
+    if (options.length > 0) {
+      setSelectedProject(options[0]);
+    }
+  }, [options]);
 
   return (
     <div className={classNames(style.header, { [style.small]: sidebarWidth < 100 })}>
@@ -45,8 +53,9 @@ export default function Logo({ sidebarWidth }: { sidebarWidth: number; }) {
               size="l"
               width="max"
               options={options}
-              onUpdate={onChange}
-              defaultValue={[project?.value]}
+              onUpdate={handleProjectChange}
+              value={[selectedProject?.content ?? '']}
+              loading
             />
             <NavLink to="/projects/add" className={style.icon}>
               <Icon data={Plus} size={20} />
