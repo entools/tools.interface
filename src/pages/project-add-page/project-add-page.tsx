461536865dced58cb1b7c370024a89f9ca221d74
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-11 19:32:01 +0300

    fix: project-add-page

--- src/pages/project-add-page/project-add-page.tsx
+++ src/pages/project-add-page/project-add-page.tsx
@@ -3,13 +3,13 @@ import { Text } from '@gravity-ui/uikit';
 import { useNavigate } from 'react-router';
 
 import { useCreateProjectMutation } from '~/store';
-import ProjectAddForm from './project-add-form';
+import ProjectForm from '../project-page/project-form';
 
 export type FormPayload = Omit<ProjectType, 'id'>;
 
 export default function ProjectPage() {
   const navigate = useNavigate();
-  const [createProject] = useCreateProjectMutation();
+  const [createProject, { isLoading }] = useCreateProjectMutation();
 
   const onSubmit = async (data: FormPayload) => {
     const result = (await createProject(data)) as unknown as { data: ProjectType };
@@ -19,7 +19,7 @@ export default function ProjectPage() {
   return (
     <div className="layout">
       <Text variant="header-2">Добавить проект</Text>
-      <ProjectAddForm onSubmit={onSubmit} />
+      <ProjectForm onSubmit={onSubmit} isLoading={isLoading} project={null} />
     </div>
   );
 }
