dec54e221bf832cae3f577c362b23a94d67f71ee
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-12 17:43:25 +0300

    fix: constants

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -3,6 +3,8 @@ export const BASE_AUTH_API_URL = 'http://localhost:3000/auth/';
 export const BASE_PROFILE_API_URL = 'http://localhost:3000/';
 export const BASE_PROJECT_API_URL = 'http://localhost:3000/';
 export const BASE_DOCUMENT_API_URL = 'http://localhost:3000/';
+export const BASE_BLOCK_API_URL = 'http://localhost:3000/';
+
 export const OAUTH_API_URL = '';
 
 export const ESC_CLOSE_ON = true;
