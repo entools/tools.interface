0f53ac7b691537792f9e20f1c747a8ea8de9f897
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-13 23:20:48 +0300

    fix: tool

--- src/pages/document-page/components/tools/tools.tsx
+++ src/pages/document-page/components/tools/tools.tsx
@@ -1,14 +1,27 @@
 import { Button, Icon } from '@gravity-ui/uikit';
+import { useNavigate } from 'react-router';
 import {
-  Eye, ArrowDownToLine, ArrowShapeTurnUpRight, ClockArrowRotateLeft,
+  Eye, ArrowDownToLine, ArrowShapeTurnUpRight, ClockArrowRotateLeft, TrashBin,
 } from '@gravity-ui/icons';
 
 import style from './tools.module.css';
 
-export default function Tools({ toggleHistory, history }
-  : { toggleHistory: () => void; history: boolean; }) {
+export default function Tools({
+  toggleHistory, history, documentId, projectId,
+}
+  : { toggleHistory: () => void; history: boolean; documentId: string; projectId: string }) {
+  const navigate = useNavigate();
+  console.log(documentId);
+
   return (
     <div className={style.tools}>
+      <Button
+        className={style.button}
+        title="Удалить"
+        onClick={() => navigate(`/projects/${projectId}`)}
+      >
+        <Icon data={TrashBin} size={16} />
+      </Button>
       <Button
         className={style.button}
         title="Подписаться"
