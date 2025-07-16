d561319ca914f7d821bfe1a70857a4ff1bb7e49c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-16 11:28:45 +0300

    fix: project-page

--- src/pages/project-page/project-page.tsx
+++ src/pages/project-page/project-page.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Button, Text } from '@gravity-ui/uikit';
+import { useNavigate, useParams } from 'react-router';
 
-import { useNavigate } from 'react-router';
 import { useAppSelector } from '~/hooks';
-import { projectSelector, useUpdateProjectMutation } from '~/store';
+import { projectSelector, useUpdateProjectMutation, useDeleteProjectMutation } from '~/store';
 
 import ProjectForm from './project-form';
 
@@ -11,8 +11,10 @@ export type FormPayload = Omit<ProjectType, 'id'>;
 
 export default function ProjectPage() {
   const navigate = useNavigate();
+  const { projectId } = useParams();
   const project = useAppSelector(projectSelector);
   const [updateProject, { isLoading }] = useUpdateProjectMutation();
+  const [deleteProject] = useDeleteProjectMutation();
 
   const onSubmit = async (data: FormPayload) => {
     if (data.name !== project?.name
@@ -22,9 +24,21 @@ export default function ProjectPage() {
     }
   };
 
+  const onDeleteProject = async () => {
+    await deleteProject(+projectId!);
+    navigate('/projects');
+  };
+
   return (
     <div className="layout">
       <Text variant="header-2">{`Проект - ${project?.name}`}</Text>
+      <Button
+        view="outlined-danger"
+        size="l"
+        onClick={onDeleteProject}
+      >
+        Удалить проект
+      </Button>
       <ProjectForm onSubmit={onSubmit} isLoading={isLoading} project={project} />
       <Button
         view="outlined-success"
