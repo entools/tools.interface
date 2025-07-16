327008850fe41582e85c20d3fc05afa3d08c314d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-16 11:30:43 +0300

    fix: document-page

--- src/pages/document-page/document-page.tsx
+++ src/pages/document-page/document-page.tsx
@@ -1,44 +1,32 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable no-console */
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import classNames from 'classnames';
-import { v4 as uuidv4 } from 'uuid';
-
-import { TextInput, Button, Icon } from '@gravity-ui/uikit';
-import { Plus } from '@gravity-ui/icons';
-
 import { Controller, useForm } from 'react-hook-form';
-import Block from './components/block/block';
+import classNames from 'classnames';
+import { TextInput } from '@gravity-ui/uikit';
+
+import Board from '../../components/board/board';
 import History from './components/history/history';
 import Tools from './components/tools/tools';
 
 import { useAppSelector } from '~/hooks';
 import {
-  useCreateBlockMutation,
   useGetDocumentBlocksMutation,
-  blockSelector,
   useGetDocumentMutation,
   documentSelector,
   useUpdateDocumentMutation,
 } from '~/store';
 
 import style from './document-page.module.css';
-import '.app.css';
 
 type FormPayload = { name: string };
 
 export default function DocumentPage() {
-  const [createBlock] = useCreateBlockMutation();
   const [getBlocks] = useGetDocumentBlocksMutation();
   const [getDocument] = useGetDocumentMutation();
   const [updateDocument] = useUpdateDocumentMutation();
 
-  const { blocks } = useAppSelector(blockSelector);
   const document = useAppSelector(documentSelector);
-
   const { projectId, documentId } = useParams();
   const [history, setHistory] = useState(false);
   const toggleHistory = () => setHistory(!history);
@@ -54,18 +42,6 @@ export default function DocumentPage() {
   const { control, reset } = useForm<FormPayload>({
     defaultValues: { name: document?.name ?? '' },
   });
-
-  const addBlock = () => {
-    createBlock({ name: `block_${blocks.length + 1}`, index: blocks.length + 1, document: { id: documentId! } });
-  };
-
-  const returnBlocksForColumn = () => blocks.map((block, index) => (
-    <Block
-      key={uuidv4()}
-      block={block}
-      index={index}
-    />
-  ));
 
   useEffect(() => {
     if (documentId) {
@@ -104,20 +80,7 @@ export default function DocumentPage() {
       </div>
       <div className={classNames(style.form, { [style.two]: history })}>
         <div className="container">
-          <DndProvider backend={HTML5Backend}>
-            {returnBlocksForColumn()}
-            <div className={style.buttons}>
-              <Button
-                title="Добавить блок"
-                view="action"
-                size="s"
-                onClick={addBlock}
-              >
-                <Icon data={Plus} size={16} />
-                Добавить блок
-              </Button>
-            </div>
-          </DndProvider>
+          <Board />
         </div>
         {history && (<History />)}
       </div>
