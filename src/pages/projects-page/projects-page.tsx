d927f790fcced2062940dcefa628448f529d219d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-18 08:13:58 +0300

    fix: projects-page

--- src/pages/projects-page/projects-page.tsx
+++ src/pages/projects-page/projects-page.tsx
@@ -1,12 +1,17 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import { Text } from '@gravity-ui/uikit';
+import { Text, List } from '@gravity-ui/uikit';
+
+import { useAppSelector } from '~/hooks';
+import { projectsSelector } from '~/store';
 
 export type FormPayload = Omit<ProjectType, 'id'>;
 
 export default function ProjectsPage() {
+  const projects = useAppSelector(projectsSelector);
+
   return (
     <div className="layout">
       <Text variant="header-2">Проекты</Text>
+      <List items={projects.map(({ name }) => name)} itemsHeight={160} />
     </div>
   );
 }
