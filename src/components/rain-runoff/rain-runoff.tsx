1151c3b6162bb09935aab0527be08c349240f486
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-18 22:27:36 +0300

    fix: rain-runoff

--- src/components/rain-runoff/rain-runoff.tsx
+++ src/components/rain-runoff/rain-runoff.tsx
@@ -1,45 +1,45 @@
 import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
 
 import RainWaterForm from '../rain-water-form/rain-water-form';
 import Modal from '../modal/modal';
 
 import style from './rain-runoff.module.css';
 
-export default function RainRunoff({ item, popupForm, handleClose }: TemplateType) {
-  const initValues = {
-    roof: '0.31',
-    pavements: '0.09',
-    tracks: '0.125',
-    ground: '0.064',
-    cobblestone: '0.145',
-    stone: '0',
-    lawns: '0.38',
-    flow: '0.224',
-    place: '1',
-    intensity: '80',
-    condition: '0',
-    // koef: 0.65,
-    timeInit: '5',
-    lengthPipe: '350',
-    lengthTray: '50',
-    velocityPipe: '0.8',
-    velocityTray: '0.7',
-  };
-  const [values, setValues] = useState<Record<string, string>>(initValues);
+const fields: (keyof RainRunoffType)[] = [
+  'roof',
+  'pavements',
+  'tracks',
+  'ground',
+  'cobblestone',
+  'stone',
+  'lawns',
+  'place',
+  'intensity',
+  'condition',
+  'timeInit',
+  'lengthPipe',
+  'lengthTray',
+  'velocityPipe',
+  'velocityTray',
+  'flow',
+];
 
+export default function RainRunoff({ item, popupForm, handleClose }: TemplateType) {
   return (
     <>
       <ul className={style.fields}>
         <li className={style.position}>{item.id}</li>
-        {Object.values(values).map((x) => (<li key={uuidv4()} className={style.field}>{x}</li>))}
+        {fields.map((x) => (<li key={uuidv4()} className={style.field}>{item.rainRunoff[x]}</li>))}
       </ul>
       {popupForm && (
       <Modal
         title="Rain Water"
         onClose={handleClose}
       >
-        <RainWaterForm data={values} setData={setValues} onClose={handleClose} />
+        <RainWaterForm
+          item={item}
+          onClose={handleClose}
+        />
       </Modal>
       )}
     </>
